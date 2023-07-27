bed8c2c32f99d4eb307ac02e46e1fb06fc50ebc1
TKuhner <trevorkuhner29@gmail.com>
2023-07-26 19:32:10 -0500

    corrected apollo server setup in server.js

--- server/server.js
+++ server/server.js
@@ -3,8 +3,20 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// create a function to initialize the ApolloServer
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +26,21 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// commented out to use ApolloServer
+// app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// integrate our ApolloServer instance with the Express application as middleware
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`)
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
+};
+
+// call the async function to start the server
+startApolloServer();
